5fc3fd13ddabc7036506c213d401621de29736ef
MartinKratochvilProgramy <martvil96@gmail.com>
2022-11-19 15:39:29 +0100

    don't display progress and full room

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-export const socket = io.connect("https://bangonlineserver-production.up.railway.app/");
+export const socket = io.connect("http://localhost:3001");
 
 // bangonlineserver-production.up.railway.app
 // http://localhost:3001
