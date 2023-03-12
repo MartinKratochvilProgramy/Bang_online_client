50590ed52fadbd35033cabb114012fefa1455d36
MartinKratochvilProgramy <martvil96@gmail.com>
2023-03-12 07:49:50 +0100

    changed socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,8 @@
 import * as io from 'socket.io-client'
 import parser from 'socket.io-msgpack-parser'
 
-// export const socket = io.connect('bangonlineserver-production.up.railway.app')
-export const socket = io.connect('http://localhost:4000', { parser })
+export const socket = io.connect('bangonlineserver-production.up.railway.app', { parser })
+// export const socket = io.connect('http://localhost:4000', { parser })
 
 // bangonlineserver-production.up.railway.app
 // http://localhost:3001
