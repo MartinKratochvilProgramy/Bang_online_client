410b4aac5eee65ac4f1acb676507d458106c5a32
MartinKratochvilProgramy <martvil96@gmail.com>
2023-04-04 20:49:49 +0200

    changed server route

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,8 @@
 import ioc from 'socket.io-client'
 import customParser from 'socket.io-msgpack-parser'
 
-// const route = 'bangonlineserver-production.up.railway.app'
-const route = 'http://localhost:4000'
+const route = 'bangonlineserver-production.up.railway.app'
+// const route = 'http://localhost:4000'
 
 export const socket = ioc(route, {
   parser: customParser
