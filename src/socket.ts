7caaf93496c1d3ae1ae9970b8e0d694465902961
MartinKratochvilProgramy <martvil96@gmail.com>
2023-10-13 13:39:44 +0200

    add aws backend

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import customParser from 'socket.io-msgpack-parser'
 
 // const route = 'bangonlineserver-production.up.railway.app'
 // const route = 'http://localhost:4000'
-const route = 'http://3.79.179.18:4000'
+const route = '3.79.179.18:4000'
 
 export const socket = ioc(route, {
   parser: customParser
