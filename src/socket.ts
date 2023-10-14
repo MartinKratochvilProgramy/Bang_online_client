8b95c98e7d5f8f50f3641f4afa8da4bc4733c06c
MartinKratochvilProgramy <martvil96@gmail.com>
2023-10-14 17:34:43 +0200

    add github action

--- src/socket.ts
+++ src/socket.ts
@@ -2,8 +2,8 @@ import ioc from 'socket.io-client'
 import customParser from 'socket.io-msgpack-parser'
 
 // const route = 'bangonlineserver-production.up.railway.app'
-// const route = 'http://localhost:4000'
-const route = '3.79.179.18:4000'
+const route = 'http://localhost:4000'
+// const route = 'https://3.79.179.18:4000'
 
 export const socket = ioc(route, {
   parser: customParser
