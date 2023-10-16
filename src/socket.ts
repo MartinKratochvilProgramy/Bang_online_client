7cb0869425c9df61bb210cc87e2d765087968fca
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-10-16 13:18:05 +0200

    add static client serving

--- src/socket.ts
+++ src/socket.ts
@@ -2,8 +2,8 @@ import ioc from 'socket.io-client'
 import customParser from 'socket.io-msgpack-parser'
 
 // const route = 'bangonlineserver-production.up.railway.app'
-// const route = 'http://localhost:443'
-const route = 'http://52.59.233.240:4000'
+const route = 'http://localhost:3000'
+// const route = 'http://52.59.233.240:4000'
 
 export const socket = ioc(route, {
   parser: customParser
