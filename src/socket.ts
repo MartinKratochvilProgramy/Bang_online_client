a2712f4070d2d53f55ee6570d718112b80de9d86
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-10-17 08:57:46 +0200

    change socket route

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import ioc from 'socket.io-client'
 import customParser from 'socket.io-msgpack-parser'
 
 // const route = 'bangonlineserver-production.up.railway.app'
-const route = 'http://localhost:3000'
+const route = 'http://localhost:5001'
 // const route = 'http://52.59.233.240:4000'
 
 export const socket = ioc(route, {
