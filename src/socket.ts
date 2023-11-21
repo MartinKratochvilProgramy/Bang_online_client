eff3c04532fab38e0e8efaff2695933b208bdf8a
MartinKratochvilProgramy <martvil96@gmail.com>
2023-11-21 08:57:22 +0100

    change backend route

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import ioc from 'socket.io-client'
 import customParser from 'socket.io-msgpack-parser'
 
 // const route = 'bangonlineserver-production.up.railway.app'
-const route = 'https://bang-online-server-dev-xdjk.1.ie-1.fl0.io/'
+const route = 'https://bang-online-server.2.ie-1.fl0.io'
 // const route = 'http://52.59.233.240:4000'
 
 export const socket = ioc(route, {
